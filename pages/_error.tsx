6025298bf031cbe0d53780570ef150ad0ccb1ced
dendidibe <mogilyk32@gmail.com>
2020-10-04 12:36:58 +0300

    Error page

---
+++ pages/_error.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import React, { ReactElement } from 'react';
+import { Layout } from "../components/layout/layout";
+import styles from '../styles/Error.module.css';
+
+function Error({ statusCode }: { statusCode: number }): ReactElement {
+    return (
+        <Layout>
+            <h3>
+                {statusCode === 404
+                    ? `Page not found`
+                    : 'An error occurred on client'}
+            </h3>
+            <Link href='/'><a className={styles.errorLink}>Go back home</a></Link>
+
+        </Layout>
+    );
+}
+
+Error.getInitialProps = ({ res, err }) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+    return { statusCode };
+};
+
+export default Error;
